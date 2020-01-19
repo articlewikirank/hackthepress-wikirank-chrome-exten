67eeb3d52cb7c1de4750f5b2db00005fc5619742
reubengt <reubengeorgethomas@gmail.com>
2020-01-19 13:23:52 +0000

    add fetch request to api, and loading animation

--- content.js
+++ content.js
@@ -1,10 +1,40 @@
-let documentClone = document.cloneNode(true);
-let article = new Readability(documentClone).parse();
-console.log(article);
-console.log("HERE IS YOUR TITLE");
-console.log(article.title);
-console.log("HERE IS YOUR TEXT");
-console.log(article.textContent);
+const wikiRankBar = document.createElement("header");
+wikiRankBar.classList.add("wikirankbar");
+const logo = document.createElement("img");
+logo.setAttribute("src", chrome.runtime.getURL("images/WikiRankLogo.png"));
+logo.classList.add("logo");
+wikiRankBar.appendChild(logo);
+const loadingAnimation = document.createElement("div");
+loadingAnimation.classList.add("lds-ellipsis");
+for (let i = 0; i < 4; i++) {
+  loadingAnimation.appendChild(document.createElement("div"));
+}
+const loadingText = document.createElement("p");
+loadingText.textContent = "Generating rankings";
+loadingText.classList.add("loading-text");
+loadingAnimation.appendChild(loadingText);
+wikiRankBar.appendChild(loadingAnimation);
 
-let wikiRankBar = document.createElement("header");
-document.body.parentNode.insertBefore(wikiRankBar, document.body.nextSibling);
+const documentClone = document.cloneNode(true);
+const article = new Readability(documentClone).parse();
+const dataToSend = {
+  text: article.title + article.textContent,
+  url: window.location.href
+};
+console.log(dataToSend);
+
+fetch("https://8491171d.ngrok.io/", {
+  method: "POST",
+  mode: "cors",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8"
+  },
+  body: JSON.stringify(dataToSend) // body data type must match "Content-Type" header
+})
+  .then(response => response.json())
+  .then(listOfKeys => {
+    wikiRankBar.removeChild(loadingAnimation);
+    console.log(listOfKeys);
+  });
+
+document.body.parentNode.insertBefore(wikiRankBar, document.body);
