ea98c3678a9af0c35ec7d82e640fcea6d1d474a8
reubengt <reubengeorgethomas@gmail.com>
2020-01-19 15:23:49 +0000

    add table for results

--- content.js
+++ content.js
@@ -10,18 +10,17 @@ for (let i = 0; i < 4; i++) {
   loadingAnimation.appendChild(document.createElement("div"));
 }
 const loadingText = document.createElement("p");
-loadingText.textContent = "Generating rankings";
-loadingText.classList.add("loading-text");
 loadingAnimation.appendChild(loadingText);
 wikiRankBar.appendChild(loadingAnimation);
 
 const documentClone = document.cloneNode(true);
 const article = new Readability(documentClone).parse();
 const dataToSend = {
-  text: article.title + article.textContent,
+  text: article.title + article.textContent.replace(/\n/g, "").trim(),
   url: window.location.href
 };
 console.log(dataToSend);
+console.log(article.textContent.replace(/\n/g, "").trim());
 
 fetch("https://8491171d.ngrok.io/", {
   method: "POST",
@@ -32,9 +31,36 @@ fetch("https://8491171d.ngrok.io/", {
   body: JSON.stringify(dataToSend) // body data type must match "Content-Type" header
 })
   .then(response => response.json())
-  .then(listOfKeys => {
-    wikiRankBar.removeChild(loadingAnimation);
-    console.log(listOfKeys);
+  .then(responseObj => {
+    document.querySelector(".wikirankbar").removeChild(loadingAnimation);
+    console.log(responseObj);
+    const requestURL = responseObj.request_url;
+    const rankingObj = responseObj.ranking;
+    const siteNamesArray = Object.keys(rankingObj);
+    const sitesTable = document.createElement("table");
+    sitesTable.classList.add("sitestable");
+    const legend = document.createElement("tr");
+    legend.innerHTML = "<tr><th>Rank</th><th>Website</th></tr>";
+    sitesTable.appendChild(legend);
+    siteNamesArray.forEach(siteName => {
+      const tableRow = document.createElement("tr");
+      tableRow.classList.add("site");
+      if (siteName === requestURL)
+        tableRow.classList.add("highlight-currentdomain");
+      const citationCount = document.createElement("td");
+      citationCount.innerText = rankingObj[siteName];
+      const name = document.createElement("td");
+      const namelink = document.createElement("a");
+      namelink.setAttribute("href", "https://" + siteName);
+      namelink.setAttribute("target", "_blank");
+      namelink.innerText = siteName;
+      name.appendChild(namelink);
+      tableRow.appendChild(name);
+      tableRow.appendChild(citationCount);
+      sitesTable.appendChild(tableRow);
+      console.log(sitesTable);
+      document.querySelector(".wikirankbar").appendChild(sitesTable);
+    });
   });
 
 document.body.parentNode.insertBefore(wikiRankBar, document.body);
